229df69f78a4b4ee72cdee24ea0d71d3d31580f9
Apurva Sharan <info@tangere.in>
2011-04-02 12:36:35 +0530

    Working after git initialization

---
+++ src/cmh/common/migrations/0001_initial.py
@@ -0,0 +1,54 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Category'
+        db.create_table('common_category', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['common.Category'], null=True, blank=True)),
+        ))
+        db.send_create_signal('common', ['Category'])
+
+        # Adding model 'Attribute'
+        db.create_table('common_attribute', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['common.Category'])),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['common.Attribute'], null=True, blank=True)),
+        ))
+        db.send_create_signal('common', ['Attribute'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Category'
+        db.delete_table('common_category')
+
+        # Deleting model 'Attribute'
+        db.delete_table('common_attribute')
+
+
+    models = {
+        'common.attribute': {
+            'Meta': {'object_name': 'Attribute'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Category']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Attribute']", 'null': 'True', 'blank': 'True'})
+        },
+        'common.category': {
+            'Meta': {'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Category']", 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['common']
