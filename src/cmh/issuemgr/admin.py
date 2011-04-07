39c41254a8c788c4351d69e5c83b9b0f003456be
Apurva Sharan <info@tangere.in>
2011-04-07 18:48:45 +0530

    complaint logging page is visually up now

--- src/cmh/issuemgr/admin.py
+++ src/cmh/issuemgr/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from cmh.issuemgr.models import ComplaintState, Complaint, ComplaintHistory
+from cmh.issuemgr.models import ComplaintState, Complaint
 
 admin.site.register(ComplaintState)
 admin.site.register(Complaint)
-admin.site.register(ComplaintHistory)
