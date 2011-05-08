f0df5d57a407a21e163995720cc81100a4e29ea1
Apurva Sharan <info@tangere.in>
2011-05-08 18:11:31 +0530

    Ability to show the events on map - initial capability

--- src/cmh/static/js/MapHandler.js
+++ src/cmh/static/js/MapHandler.js
@@ -1,6 +1,6 @@
 var MapHandler = {
     map : null,
-    init : function (map_canvas, center_lat, center_long, zoom_level) {
+    init : function (map_canvas, center_lat, center_long, zoom_level, callback) {
         var latlng = new google.maps.LatLng(center_lat, center_long);
         var myOptions = {
             zoom: zoom_level,
@@ -8,6 +8,7 @@ var MapHandler = {
             mapTypeId: google.maps.MapTypeId.HYBRID
         };
         this.map = new google.maps.Map(document.getElementById(map_canvas), myOptions);
+        callback ();
     },
     placeAlert : function (place) {
         return (function x (){
@@ -19,9 +20,11 @@ var MapHandler = {
         $.getJSON (url + category + "/",
                    null,
                    function (data) {
+                       var bounds = new google.maps.LatLngBounds ();
                        $.each (data, function (index, value) {
                                    var place_latlong = new google.maps.LatLng (value.latitude,
                                                                                value.longitude);
+                                   bounds.extend (place_latlong);
                                    
                                    var circleOpts = {
                                        strokeColor: "#FF0000",
@@ -31,10 +34,12 @@ var MapHandler = {
                                        fillOpacity: 0.35,
                                        map: MapHandler.map,
                                        center: place_latlong,
-                                       radius: value.count * 25
+                                       radius: value.count * 200
                                    };
                                    var placeCircle = new google.maps.Circle (circleOpts);
                                });
+                       MapHandler.map.setCenter (bounds.getCenter ());
+                       MapHandler.map.fitBounds (bounds);
                        // google.maps.event.addListener (placeCircle, 
                        //                                'click',
                        //                                MapHandler.placeAlert (place));
