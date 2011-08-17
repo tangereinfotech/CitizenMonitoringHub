d402b1b07a7679d66503c8a3aecc8898143758af
tangereinfotech <info@tangere.in>
2011-08-17 16:52:23 +0530

    First cut hindi localization, showing phone numbers from settings

---
+++ src/cmh/common/templatetags/common_extras.py
@@ -0,0 +1,36 @@
+# encoding: utf-8
+# Copyright 2011, Tangere Infotech Pvt Ltd [http://tangere.in]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from django import template
+from django.conf import settings
+
+register = template.Library ()
+
+@register.tag
+def value_from_settings (parser, token):
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, var = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents.split()[0]
+    return ValueFromSettings (var)
+
+class ValueFromSettings (template.Node):
+    def __init__ (self, var):
+        self.arg = var
+
+    def render (self, context):
+        return settings.__getattr__(self.arg)
