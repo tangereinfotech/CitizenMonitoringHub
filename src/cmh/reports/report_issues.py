0c1f0ef12bc7b196129a80f93387483bab917c50
Pankaj Lal <pankaj@tangere.in>
2012-12-18 18:28:11 +0530

    updated with better caching on all issues

--- src/cmh/reports/report_issues.py
+++ src/cmh/reports/report_issues.py
@@ -1,7 +1,7 @@
 from cmh.issuemgr.models import Complaint, ComplaintEvidence, ComplaintReminder, ComplaintClosureMetric, ComplaintManager
 from django.utils.translation import ugettext as _
 from django.core.cache import cache
-from django.utils.simplejson import dumps
+from django.utils.simplejson import dumps,loads
 from django.http import HttpResponse,Http404
 from cmh.common.constants import UserRoles
 from django.core.exceptions import ObjectDoesNotExist
@@ -104,6 +104,8 @@ def get_reminder(c,request):
         reminder = ComplaintReminder.objects.get(complaintno = c.complaintno, user = request.user)
         return reminder.reminderon.strftime("%y.%m.%d")
     except ObjectDoesNotExist:
+        return ''
+    except:
         return ''
 
 def get_department_select():
@@ -275,41 +277,60 @@ all_issues_column_properties = {
     }
 }
 
+from datetime import datetime
 def report_all_issues_data(request):
-#    mdata = cache.get('all_issues_column_key')
-#    if mdata == None:
-    cdata = []
-    latest_complaints = Complaint.objects.filter(latest = True)
-    for comp in latest_complaints:
-        row = {}
-        for i in range(0,len(all_issues_column_properties)):
-            row[str(i)] = all_issues_column_properties[i]['fnGetData'](comp,request)
-        row["DT_RowID"] = str(comp.complaintno)
-        cdata.append(row)
-    mdata = dumps({'aaData': cdata})
-#   cache.set('all_issues_column_key', mdata,24*60*60)
-    return HttpResponse(mdata)
+    mdata = cache.get('all_issues_column_key')
+    if mdata == None:
+        cdata = []
+        latest_complaints = Complaint.objects.filter(latest = True)
+        num_columns = len(all_issues_column_properties)
+        range_columns = range(0,num_columns)
+        for comp in latest_complaints:
+            row = {}
+            for i in range_columns:
+                row[str(i)] = all_issues_column_properties[i]['fnGetData'](comp,request)
+            row["DT_RowID"] = str(comp.complaintno)
+            cdata.append(row)
+        mdata = cdata
+        cache.set('all_issues_column_key', mdata,24*60*60)
+    return HttpResponse(dumps({'aaData': mdata}))
 
 def report_my_issues_data(request):
+
     role = request.user.cmhuser.get_user_role()
     if (role == UserRoles.ROLE_OFFICIAL or role == UserRoles.ROLE_DELEGATE):
         official = request.user.official
- #       mdata = cache.get('my_issues_column_key_' + request.user.username)
- #       if mdata == None:
-        cdata = []
-        latest_complaints = Complaint.objects.filter(latest = True, department = official.department)
-        for comp in latest_complaints:
-            row = {}
-            for i in range(0,len(all_issues_column_properties)):
-                row[str(i)] = all_issues_column_properties[i]['fnGetData'](comp,request)
-            row["DT_RowID"] = "row_" + str(i)
-            cdata.append(row)
-        mdata = dumps({'aaData': cdata})
-        #cache.set('my_issues_column_key_' + request.user.username, mdata,24*60*60)
+        mdata = cache.get('my_issues_column_key_' + request.user.username)
+        if mdata == None:
+            cdata = []
+            latest_complaints = Complaint.objects.filter(latest = True, department = official.department)
+            for comp in latest_complaints:
+                row = {}
+                for i in range(0,len(all_issues_column_properties)):
+                    row[str(i)] = all_issues_column_properties[i]['fnGetData'](comp,request)
+                row["DT_RowID"] = str(comp.complaintno)
+                cdata.append(row)
+            mdata = dumps({'aaData': cdata})
+        cache.set('my_issues_column_key_' + request.user.username, mdata,24*60*60)
         return HttpResponse(mdata)
     elif (role == UserRoles.ROLE_DM or UserRoles.ROLE_CSO):
         return report_all_issues_data(request)
     else:
         raise Http404
 
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+@receiver(post_save, sender = Complaint)
+def update_cache_data_all_issues_complaint(sender, **kwargs):
+    mdata = cache.get('all_issues_column_key')
+    if (mdata != None):
+        for row_dic in mdata:
+            if row_dic["DT_RowID"] == str(kwargs['instance'].complaintno):
+                comp = kwargs['instance']
+                try:
+                    comp = Complaint.objects.get(complaintno = comp.complaintno, latest = True)
+                    for i in range(0, len(all_issues_column_properties)):
+                        row_dic[str(i)] = all_issues_column_properties[i]['fnGetData'](comp,None)
+                except ObjectDoesNotExist:
+                    pass
+        cache.set('all_issues_column_key',mdata)
