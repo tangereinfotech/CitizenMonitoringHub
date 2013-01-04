88871399eb125406b4a2f8aede1328fba30122b5
Pankaj Lal <pankaj@tangere.in>
2013-01-04 18:34:24 +0530

    solved the inconsistency in reports

--- src/cmh/reports/report_issues.py
+++ src/cmh/reports/report_issues.py
@@ -16,7 +16,7 @@ def get_complaint_no(c,request=None):
 
 def get_filed_on(c, request=None, fmt="%Y.%m.%d"):
     if ((c is not None) and (c.original is not None) and (c.original.logdate is not None)):
-        return c.original.logdate.strftime(fmt)
+        return c.logdate.strftime(fmt)
     else:
         return ''
 
@@ -359,49 +359,51 @@ def report_my_issues_data(request):
     else:
         raise Http404
 
-def create_or_update_idr(comp):
-    try:
-        idr = IssuesDataReport.objects.get(complaintno = comp.complaintno)
-        idr.complaintno = get_complaint_no(comp)
-        idr.filed_on = get_filed_on(comp)
-        idr.last_updated = get_last_updated(comp)
-        idr.department = get_department(comp)
-        idr.department_name = get_department_name(comp)
-        idr.filed_by = get_filed_by(comp)
-        idr.location= get_location(comp)
-        idr.description = get_description(comp)
-        idr.latest_update = get_latest_update(comp)
-        idr.accepted_by = get_accepted_by(comp)
-        idr.last_updated_by = get_last_updated_by(comp)
-        idr.complaint_status = get_workflow_state(comp)
-        idr.attachments = get_attachments(comp)
-        idr.action = get_action(comp)
-        idr.save()
-    except ObjectDoesNotExist:
-        IssuesDataReport.objects.create(complaintno = get_complaint_no(comp),
-                                        filed_on = get_filed_on(comp),
-                                        last_updated = get_last_updated(comp),
-                                        department = get_department(comp),
-                                        department_name = get_department_name(comp),
-                                        filed_by = get_filed_by(comp),
-                                        location= get_location(comp),
-                                        description = get_description(comp),
-                                        latest_update = get_latest_update(comp),
-                                        accepted_by = get_accepted_by(comp),
-                                        last_updated_by = get_last_updated_by(comp),
-                                        complaint_status = get_workflow_state(comp),
-                                        attachments = get_attachments(comp),
-                                        action = get_action(comp)
-                                        )
+def create_or_update_idr(ch_comp):
+    if ch_comp.complaintno:
+        cs = Complaint.objects.filter(complaintno = ch_comp.complaintno).filter(latest = True)
+        if cs.count() == 1:
+            comp = cs[0]
+            try:
+                idr = IssuesDataReport.objects.get(complaintno = comp.complaintno)
+                idr.complaintno = get_complaint_no(comp)
+                idr.filed_on = get_filed_on(comp)
+                idr.last_updated = get_last_updated(comp)
+                idr.department = get_department(comp)
+                idr.department_name = get_department_name(comp)
+                idr.filed_by = get_filed_by(comp)
+                idr.location= get_location(comp)
+                idr.description = get_description(comp)
+                idr.latest_update = get_latest_update(comp)
+                idr.accepted_by = get_accepted_by(comp)
+                idr.last_updated_by = get_last_updated_by(comp)
+                idr.complaint_status = get_workflow_state(comp)
+                idr.attachments = get_attachments(comp)
+                idr.action = get_action(comp)
+                idr.save()
+            except ObjectDoesNotExist:
+                IssuesDataReport.objects.create(complaintno = get_complaint_no(comp),
+                                                filed_on = get_filed_on(comp),
+                                                last_updated = get_last_updated(comp),
+                                                department = get_department(comp),
+                                                department_name = get_department_name(comp),
+                                                filed_by = get_filed_by(comp),
+                                                location= get_location(comp),
+                                                description = get_description(comp),
+                                                latest_update = get_latest_update(comp),
+                                                accepted_by = get_accepted_by(comp),
+                                                last_updated_by = get_last_updated_by(comp),
+                                                complaint_status = get_workflow_state(comp),
+                                                attachments = get_attachments(comp),
+                                                action = get_action(comp)
+                                                )
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 @receiver(post_save, sender = Complaint)
 def update_all_issues_complaint_report(sender, **kwargs):
     comp = kwargs['instance']
-    if kwargs['created'] == False:
-        if comp.latest == True:
-            create_or_update_idr(comp)
+    create_or_update_idr(comp)
 
 @receiver(post_save, sender = ComplaintDepartment)
 def update_all_issues_complaint_department(sender, **kwargs):
